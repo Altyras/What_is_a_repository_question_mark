5e4f4ef6f0d7a72347fae09a8517c24452ebbba4
Altyras <dcommando05@gmail.com>
2024-11-03 20:20:24 +0000

    Yey I memorized Array, time to sleep

---
+++ Java/JavaTempMem.java
@@ -0,0 +1,7 @@
+public class JavaTempMem {
+    public static void main(String[] args) {
+        int[][] stuff={{1,2,3,4},{2,3,4,5},{3,4,5,6}};
+        for(int i=0;i<stuff.length;i++){
+            for(int j=0;j<stuff[i].length;j++){
+                System.out.print(stuff[i][j] + " ");
+}   }   }   }
