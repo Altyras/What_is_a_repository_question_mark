141b12754d2c2c6c341516922096fea300b62dd5
Altyras <dcommando05@gmail.com>
2024-11-03 20:15:38 +0000

    OKAY NOW IT FINALLY WORKS

---
+++ Java/JavaArray.java
@@ -0,0 +1,7 @@
+public class JavaArray{
+    public static void main(String[] args){
+        int[][] stuff={{1,2,3,4},{5,6,7,8},{9,10,11,12}};
+        for(int i=0;i<stuff.length;i++){
+            for(int j=0;j<stuff[0].length;j++){
+                System.out.print(stuff[i][j]+" ");
+}   }   }   }
